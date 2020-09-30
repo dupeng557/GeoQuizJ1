a3c4fe8db3ae74cbb8ad9264874a341d82f8c7f4
Dupeng <dupengnlfz@sina.com>
2020-09-30 10:38:39 +0800

    first commit

---
+++ app/src/main/java/com/bignerd/android/geoquizj1/Question.java
@@ -0,0 +1,26 @@
+package com.bignerd.android.geoquizj1;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
